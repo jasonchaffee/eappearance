2080635e6763abc54538e83184c2fc60dc3fe9ff
Jason Chaffee <jasonchaffee@gmail.com>
2014-11-08 03:37:57 -0800

    customized secure social

--- app/Global.scala
+++ app/Global.scala
@@ -1,9 +1,11 @@
-//import controllers.CustomRoutesService
+import controllers.CustomRoutesService
 
 import java.lang.reflect.Constructor
 
 import securesocial.core.RuntimeEnvironment
+import securesocial.core.providers._
 import service.{DemoUser, InMemoryUserService, MyEventListener}
+import scala.collection.immutable.ListMap
 
 object Global extends play.api.GlobalSettings {
 
@@ -11,9 +13,28 @@ object Global extends play.api.GlobalSettings {
    * The runtime environment for this sample app.
    */
   object MyRuntimeEnvironment extends RuntimeEnvironment.Default[DemoUser] {
-    //    override lazy val routes = new CustomRoutesService()
+    override lazy val routes = new CustomRoutesService()
     override lazy val userService: InMemoryUserService = new InMemoryUserService()
     override lazy val eventListeners = List(new MyEventListener())
+
+    override lazy val providers = ListMap(
+      // oauth 2 client providers
+      include(new FacebookProvider(routes, cacheService, oauth2ClientFor(FacebookProvider.Facebook))),
+      //include(new FoursquareProvider(routes, cacheService,oauth2ClientFor(FoursquareProvider.Foursquare))),
+      //include(new GitHubProvider(routes, cacheService,oauth2ClientFor(GitHubProvider.GitHub))),
+      include(new GoogleProvider(routes, cacheService,oauth2ClientFor(GoogleProvider.Google))),
+      //include(new InstagramProvider(routes, cacheService,oauth2ClientFor(InstagramProvider.Instagram))),
+      //include(new LinkedInOAuth2Provider(routes, cacheService,oauth2ClientFor(LinkedInOAuth2Provider.LinkedIn))),
+      //include(new VkProvider(routes, cacheService,oauth2ClientFor(VkProvider.Vk))),
+      //include(new DropboxProvider(routes, cacheService, oauth2ClientFor(DropboxProvider.Dropbox))),
+      //include(new WeiboProvider(routes, cacheService, oauth2ClientFor(WeiboProvider.Weibo))),
+      // oauth 1 client providers
+      include(new LinkedInProvider(routes, cacheService, oauth1ClientFor(LinkedInProvider.LinkedIn))),
+      include(new TwitterProvider(routes, cacheService, oauth1ClientFor(TwitterProvider.Twitter))),
+      //include(new XingProvider(routes, cacheService, oauth1ClientFor(XingProvider.Xing))),
+      // username password
+      include(new UsernamePasswordProvider[DemoUser](userService, avatarService, viewTemplates, passwordHashers))
+    )
   }
 
   /**
