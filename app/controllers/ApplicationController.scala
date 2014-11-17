9834b59cc44e9eb5638c6a4037c15865e0f42181
Jason Chaffee <jasonchaffee@gmail.com>
2014-11-17 07:02:26 -0800

    trying to get heroku working

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -23,7 +23,7 @@ class ApplicationController @Inject() (implicit val env: Environment[User, Cache
    *
    * @return The result to display.
    */
-  def index = UserAwareAction.async { implicit request =>
+  def index = SecuredAction.async { implicit request =>
     val list = List(
       ArraignmentPlea(UUID.randomUUID(), "8398098", Defendant(UUID.randomUUID(), Some("Jason"), Some("Chaffee"), Some("Jason Chaffee")), LocalDateTime.now()),
       ArraignmentPlea(UUID.randomUUID(), "3409893", Defendant(UUID.randomUUID(), Some("Brad"), Some("Koehn"), Some("Brad Koehn")), LocalDateTime.now()),
